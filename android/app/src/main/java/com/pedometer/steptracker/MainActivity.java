ac10becdf17f8b6b45675dd13914b1dae595bb7c
Yash <yv48183@gmail.com>
2023-11-20 14:25:08 +0530

    New push is happened

--- android/app/src/main/java/com/pedometer/steptracker/MainActivity.java
+++ android/app/src/main/java/com/pedometer/steptracker/MainActivity.java
@@ -1,7 +1,7 @@
 package com.pedometer.steptracker;
 
 import io.flutter.embedding.android.FlutterActivity;
-
+import android.util.Log;
 import androidx.annotation.NonNull;
 import android.os.Bundle;
 
@@ -17,8 +17,10 @@ public class MainActivity extends FlutterActivity {
     }
 
     private void funToBeCalledFromFlutterSDK() {
+        Log.d("New Sdk nasir","BEfore");
         AnalyticsTracker.getInstance().start(getApplicationContext(), "EVJ5aBod18IUuAfZr6bK");
 //        AnalyticsTracker.getInstance().setDebugLog(true);
+        Log.d("New Sdk nasir","After");
     }
 
 }
